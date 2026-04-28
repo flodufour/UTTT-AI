7880c17695305c26310ca5bf755cefaaa5418861
flo404x <floriandufour66@gmail.com>
2026-04-28 17:56:42 +0200

    remove randomStrat attributes

--- UTTT/include/ai/strategy/RandomStrategy.h
+++ UTTT/include/ai/strategy/RandomStrategy.h
@@ -7,8 +7,5 @@ public:
     AIMove chooseMove(const GameState& state) override;
 
 private:
-    bool _isWinningMove(const GameState& state, const AIMove& move);
-    bool _isBlockingMove(const GameState& state, const AIMove& move);
-
     Evaluation _eval;
 };
