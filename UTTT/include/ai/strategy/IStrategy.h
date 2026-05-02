aeadd03daeb63621b7dc2a87c5cf4c681c1438c2
flo404x <floriandufour66@gmail.com>
2026-05-02 14:32:25 +0200

    doc for IStrategy

--- UTTT/include/ai/strategy/IStrategy.h
+++ UTTT/include/ai/strategy/IStrategy.h
@@ -3,8 +3,19 @@
 #include "core/AIMove.h"
 #include "core/GameState.h"
 
+
+
+/// @class IStrategy
+/// @brief Interface for AI move selection strategies.
+///
+/// This interface defines a common contract for all AI strategies
 class IStrategy {
 public:
+    /// @brief Selects the best move for the given game state.
+    /// @param state Current game state.
+    /// @return Chosen move for the AI.
     virtual AIMove chooseMove(const GameState& state) = 0;
+
+    /// @brief Virtual destructor for safe polymorphic deletion.
     virtual ~IStrategy() = default;
 };
